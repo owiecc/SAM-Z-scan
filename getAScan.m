1d07931328b90ca734d2c47f33828a37e42f3daa
owiecc <szymon@h103.aau1x.klient.pon.site.aau.dk>
2016-04-29 14:45:30 +0200

    AScan

--- getAScan.m
+++ getAScan.m
@@ -1,6 +1,5 @@
 function [ aScan ] = getAScan( zData,x,y )
-%UNTITLED3 Summary of this function goes here
+%getAScan Summary of this function goes here
 %   Detailed explanation goes here
 aScan = double(zData(:,x,y));
 end
-
